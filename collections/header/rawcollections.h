fd09a90bc6ba3059e5d5ee5341f9516002a6fd21
Brook <brook.smith@fronde.com>
2021-07-24 16:21:53 +1200

    rename

---
+++ collections/header/rawcollections.h
@@ -0,0 +1,13 @@
+#ifndef __RAWCOLLECTIONS_HH
+#define __RAWCOLLECTIONS_HH
+
+#include "data-types.h"
+#include "object-types.h"
+#include "quicksort.h"
+#include "rawarraylist.h"
+#include "rawhashmap.h"
+#include "rawbitslist.h"
+#include "rawlinkedlist.h"
+#include "rawtree.h"
+
+#endif
