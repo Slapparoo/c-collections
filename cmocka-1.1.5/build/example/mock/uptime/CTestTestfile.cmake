d5519a15fbda1e1640f506800e901eae245adcb4
Brook Smith <me.brook.smith@gmail.com>
2021-08-06 13:44:38 +1200

    Delete cmocka-1.1.5 directory

--- cmocka-1.1.5/build/example/mock/uptime/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/brook/source/brooks-object-c/src/cmocka-1.1.5/example/mock/uptime
-# Build directory: /home/brook/source/brooks-object-c/src/cmocka-1.1.5/build/example/mock/uptime
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test_uptime "test_uptime")
-set_tests_properties(test_uptime PROPERTIES  _BACKTRACE_TRIPLES "/home/brook/source/brooks-object-c/src/cmocka-1.1.5/cmake/Modules/AddCMockaTest.cmake;116;add_test;/home/brook/source/brooks-object-c/src/cmocka-1.1.5/example/mock/uptime/CMakeLists.txt;12;add_cmocka_test;/home/brook/source/brooks-object-c/src/cmocka-1.1.5/example/mock/uptime/CMakeLists.txt;0;")
